e1a6f13e32e09497da141056fab1bb361492c0e2
Sylvester-agbo <71844379+Sylvester-agbo@users.noreply.github.com>
2024-03-05 03:23:04 -0700

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,16 +1,9 @@
 variable "env" {
   type    = string
-  default = "Dev"
 }
 
 variable "region" {
   type    = string
-  default = "us-west-2"
-}
-
-variable "versioning" {
-  type    = string
-  default = "Enabled"
 }
 
 variable "create_vpc" {
